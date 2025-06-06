1b5409f09ebcadc9b3f4cb2cda0078eef473638e
Jia Hao Christian Chen <jiahao@itsincom.it>
2025-06-06 14:33:32 +0200

    card

--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
-import Image from "next/image";
 import Hero from "@/components/Hero";
+import Products from "@/components/Products";
 
 export default function Home() {
     return (
         <div>
             <Hero />
+            <Products />
         </div>
     );
 }
